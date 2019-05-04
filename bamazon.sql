dc2718dcc7164cbeb8b70d54c6241b8512cf1321
JDC-RVA <jeremy@firstmotionlc.com>
2019-05-03 21:03:23 -0400

    added user prompt logic

--- bamazon.sql
+++ bamazon.sql
@@ -11,3 +11,17 @@ CREATE TABLE products (
   stock_quantity INT(10) NOT NULL,
   PRIMARY KEY (item_id)
 );
+
+Select * FROM products;
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) 
+VALUES (1, "Game Of Thrones", "dvd", 69.99, 20),
+	   (2, "Wilson Basketball", "sports", 59.99, 10),
+	   (3, "Logitech Mouse", "electronics", 29.99, 20),
+	   (4, "iPhone", "electronics", 999.99, 20),
+	   (5, "Apple Watch", "electronic", 325.99, 12),
+	   (6, "Levi Jeans", "apparel", 39.99, 10),
+	   (7, "Ray-Ban Sunglasses", "accessories", 89.99, 15),
+	   (8, "Macbook Pro", "electronics", 1999.99, 5),
+	   (9, "Sunblock", "topicals", 7.99, 20),
+	   (10, "Samsung Galaxy", "electronics", 499.99, 10)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,7 +15,70 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
+// console log connection ID
 connection.connect(function(err) {
   if (err) throw err;
-  runSearch();
+  console.log("connected as id " + connection.threadId + "\n");
+  readProducts();
 });
+
+function readProducts() {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    var display = "";
+    for (var i = 0; i < res.length; i++) {
+      display = "";
+      display += "Item ID: " + res[i].item_id + "  ~  ";
+      display += "Product Name: " + res[i].product_name + "  ~  ";
+      display += "Department: " + res[i].department_name + "  ~  ";
+      display += "Price: $" + res[i].price + "\n";
+
+      console.log(display);
+    }
+    userPrompt();
+  });
+}
+
+function userPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "item_id",
+        message: "What would you like to buy? Please enter in product item ID"
+      },
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many do you need?"
+      }
+    ])
+    .then(function(input) {
+      var itemID = input.item_id;
+      var quantity = input.quantity;
+      connection.query(
+        `SELECT stock_quantity, price FROM products WHERE item_id = ${itemID}`,
+        function(err, res) {
+          if (err) throw err;
+          // console.log(res)
+
+          var availableQuantity =
+            parseInt(res[0].stock_quantity) - parseInt(quantity);
+          var price = res[0].price;
+          if (availableQuantity > 0) {
+            connection.query(
+              `UPDATE products SET stock_quantity = ${availableQuantity} WHERE item_id = ${itemID}`,
+              function(err, res) {
+                console.log(`Your total is: ${price * quantity}`);
+                connection.end();
+              }
+            );
+          } else {
+            console.log("Insufficient quantity!");
+            connection.end();
+          }
+        }
+      );
+    });
+}
